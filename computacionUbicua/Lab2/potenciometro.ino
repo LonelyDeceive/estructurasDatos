e9d226c105c5da0fdb6cb322021e705264ef4b62
LonelyDeceive <jmario.carvajalde@gmail.com>
2026-02-14 15:39:48 -0500

    Lab 2

---
+++ computacionUbicua/Lab2/potenciometro.ino
@@ -0,0 +1,13 @@
+int pinLED = 3; 
+int pinPOT = A0; 
+
+void setup() { 
+  pinMode(pinLED, OUTPUT);
+} 
+
+void loop() { 
+  int valor = analogRead(pinPOT);
+  int PWM = map(valor, 0, 1023, 0, 255);
+  analogWrite(pinLED, PWM);
+  delay(50);
+} 
